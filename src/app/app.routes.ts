e8054d7a1d4116f9ff127d501c426f6c7bcbc90f
mrmemar <mrmemar1995@gmail.com>
2024-03-11 23:59:00 +0330

    fix product routes bugs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { ListComponent } from './pages/products/list/list.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-    { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+    // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
     { path: "products", loadChildren: () => import('./pages/products/products.routes').then(r => r.ROUTES) }
 
 
--- src/app/pages/products/products.routes.ts
+++ src/app/pages/products/products.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
-import { WelcomeComponent } from '../welcome/welcome.component';
 
 export const ROUTES: Routes = [
     { path: 'list', component: ListComponent },
-    { path: 'add', component: WelcomeComponent },
 ];
