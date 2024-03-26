e67fc3561cd884e2106a82910d07a7b1bd37bc85
mrmemar <mrmemar1995@gmail.com>
2024-03-27 00:00:28 +0330

    create input section and fix form section

--- src/app/pages/common/form/form.component.ts
+++ src/app/pages/common/form/form.component.ts
@@ -1,40 +1,27 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormArrayName, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzInputModule } from 'ng-zorro-antd/input';
 import { Form } from '../../../models/form.model';
-import { NzUploadFile, NzUploadModule } from 'ng-zorro-antd/upload';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { NgIf } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzModalModule } from 'ng-zorro-antd/modal';
 import { BehaviorSubject } from 'rxjs';
+import { InputComponent } from '../input/input.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
     selector: 'app-form',
     standalone: true,
-    imports: [NzFormModule, NzInputModule, ReactiveFormsModule, NzUploadModule,
-        NgIf, NzIconModule, NzSelectModule, NzModalModule, NzButtonModule],
+    imports: [NzFormModule, ReactiveFormsModule, InputComponent, NzButtonModule],
     templateUrl: './form.component.html',
     styleUrl: './form.component.css'
 })
 export class FormComponent implements OnInit, OnChanges {
-    @Input() forms: Form[] = [];
+    @Input() formItems: Form[] = [];
     @Input() model: any;
     @Input() clear: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     @Output() save = new EventEmitter();
-
-    customForm: FormGroup = {} as FormGroup;
     loading = false;
-    avatarUrl?: string;
-    imageUrl?: string;
-    fileList: NzUploadFile[] = []
-    previewImage: string | undefined = '';
-    previewVisible = false;
+    customForm: FormGroup = {} as FormGroup;
 
-    constructor(private route: ActivatedRoute, private msg: NzMessageService) { }
+    constructor(private route: ActivatedRoute) { }
 
     ngOnInit(): void {
         if (this.route.snapshot.params["id"]) {
@@ -44,23 +31,27 @@ export class FormComponent implements OnInit, OnChanges {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        // edit mode
         if (changes['model'] && changes['model'].currentValue && !changes['model'].firstChange) {
-            this.fillForm(changes['model'].currentValue)
+            this.loading = false;
         }
-
     }
 
     createForm() {
         const formControllers: any = {};
-        this.forms.forEach(form => {
+        this.formItems.forEach(form => {
             let rulles: any[] = [];
             form.rules?.forEach(rull => {
                 if (rull == "required") {
                     rulles.push(Validators.required);
                 }
             });
-            formControllers[form.name] = new FormControl("", rulles)
+            if (form.type == "imageArray") {
+                formControllers[form.name] = new FormArray([])
+                console.log()
+            } else {
+                formControllers[form.name] = new FormControl("", rulles)
+            }
+
         })
         this.customForm = new FormGroup(formControllers);
         this.clear.subscribe(q => {
@@ -70,77 +61,23 @@ export class FormComponent implements OnInit, OnChanges {
         })
     }
 
-    clearForm() {
-        this.avatarUrl = "";
-        this.imageUrl = "";
-        this.fileList = [];
-        this.customForm.reset();
-        this.clear.next(false);
-        this.loading = false;
-    }
-
-    fillForm(model: any) {
-        this.customForm.patchValue(model);
-        this.avatarUrl = model.image;
-        if (model.images && model.images.length) {
-            this.fileList = [];
-            model.images.forEach((img: string) => {
-                this.fileList.push({
-                    name: img.split("/")[img.split("/").length - 1],
-                    uid: img.split("/")[img.split("/").length - 1],
-                    url: img
-                })
-            })
-        }
-        this.loading = false;
-    }
-
     _save() {
+        console.log(this.customForm.value)
         if (this.customForm.valid) {
             this.loading = true;
-            if (this.imageUrl) {
-                this.customForm.value['image'] = this.imageUrl;
-            }
-            if (this.fileList.length > 0) {
-                this.customForm.value['images'] = []
-                this.fileList.forEach(file => {
-                    if (file.response) {
-                        this.customForm.value['images'].push(file.response.location);
-                    } else {
-                        this.customForm.value['images'].push(file.url);
-                    }
-                })
-            }
-
             this.save.emit(this.customForm.value);
             this.loading = false;
         }
     }
 
-    private getBase64(img: File, callback: (img: string) => void): void {
-        const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result!.toString()));
-        reader.readAsDataURL(img);
+    clearForm() {
+        // this.avatarUrl = "";
+        // this.imageUrl = "";
+        // this.fileList = [];
+        this.customForm.reset();
+        this.clear.next(false);
+        this.loading = false;
     }
 
-    handleChange(info: { file: NzUploadFile }): void {
-        this.loading = true
-        switch (info.file.status) {
-            case 'uploading':
-                this.loading = true;
-                break;
-            case 'done':
-                // Get this url from response in real world.
-                this.getBase64(info.file!.originFileObj!, (img: string) => {
-                    this.avatarUrl = img;
-                    this.imageUrl = info.file.response.location;
-                    this.loading = false;
-                });
-                break;
-            case 'error':
-                this.msg.error('Network error');
-                this.loading = false;
-                break;
-        }
-    }
+
 }
--- src/app/pages/common/input/input.component.ts
+++ src/app/pages/common/input/input.component.ts
@@ -1,12 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { Form } from '../../../models/form.model';
+import { NzUploadFile, NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { JsonPipe, NgIf } from '@angular/common';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-input',
-  standalone: true,
-  imports: [],
-  templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+    selector: 'app-input',
+    standalone: true,
+    imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzSelectModule, NzUploadModule, NgIf,
+        NzIconModule, JsonPipe],
+    templateUrl: './input.component.html',
+    styleUrl: './input.component.css'
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
+    @Input() formGroup: FormGroup<any> = {} as FormGroup;
+    @Input() formItem: Form = {} as Form;
+    @Input() formItemValue: any;
+    @Input() clear: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    loading = false;
+    imageUrl?: string;
+    fileList: NzUploadFile[] = []
+
+    constructor(private msg: NzMessageService) {
+    }
+
+    ngOnInit(): void {
+        this.clear.subscribe(q => {
+            if (q) {
+                this.fileList = [];
+            }
+        })
+        // console.log(this.formItem)
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        // edit mode
+        if (changes['formItemValue'] && changes['formItemValue'].currentValue && !changes['formItemValue'].firstChange) {
+            const newValue = changes['formItemValue'].currentValue
+            if (this.formItem.type == "imageArray") {
+                this.fileList = [];
+                newValue.forEach((img: string) => {
+                    this.fileList.push({
+                        name: img.split("/")[img.split("/").length - 1],
+                        uid: img.split("/")[img.split("/").length - 1],
+                        url: img
+                    })
+                })
+            }
+            const value: any = {};
+            value[this.formItem.name] = newValue;
+            // if form array
+            if (this.formItem.name == 'images') {
+                value.images.forEach(() => {
+                    this.getFormArray('images').push(new FormControl());
+                })
+            }
+            this.formGroup.patchValue(value);
+        }
+
+    }
+
+    fileChange(info: { file: NzUploadFile }): void {
+        this.loading = true
+        switch (info.file.status) {
+            case 'uploading':
+                this.loading = true;
+                break;
+            case 'done':
+                this.imageUrl = info.file.response.location;
+                this.formGroup.patchValue({
+                    'image': this.imageUrl
+                })
+                this.loading = false;
+                break;
+            case 'error':
+                this.msg.error('Network error');
+                this.loading = false;
+                break;
+        }
+    }
+
+    getFormArray(name: string): FormArray<any> {
+        return this.formGroup.get(name) as FormArray<any>;
+    }
+
+    fileListChange(listName: string, info: { fileList: NzUploadFile[], file: NzUploadFile }) {
+        this.getFormArray(listName).clear();
+        const files: string[] = []
+        info.fileList.forEach(file => {
+            this.getFormArray(listName).push(new FormControl());
+            if (file.response) {
+                files.push(file.response.location)
+            } else if (file.url) {
+                files.push(file.url)
+            }
+        })
+        this.getFormArray(listName).patchValue(files)
+    }
+
 
 }
