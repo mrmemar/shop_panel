604cb793413cdedf5f9fa558e8c18350975d0868
mrmemar <mrmemar1995@gmail.com>
2024-03-27 13:36:58 +0330

    create table-cell component

---
+++ src/app/pages/common/table-cell/table-cell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-cell',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-cell.component.html',
+  styleUrl: './table-cell.component.css'
+})
+export class TableCellComponent {
+
+}
