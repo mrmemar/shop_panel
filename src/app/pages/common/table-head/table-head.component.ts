a62eccc8a5c0104143cc003b8b7aca9a6b3d735d
mrmemar <mrmemar1995@gmail.com>
2024-03-27 16:33:42 +0330

    create table-head component

---
+++ src/app/pages/common/table-head/table-head.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-head',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-head.component.html',
+  styleUrl: './table-head.component.css'
+})
+export class TableHeadComponent {
+
+}
