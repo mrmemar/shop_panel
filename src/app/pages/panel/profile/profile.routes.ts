6d9627a7441e013a745baf14b6d15b625f2b26d5
mrmemar <mrmemar1995@gmail.com>
2024-04-03 00:09:22 +0330

    remove extra files from  project

--- src/app/pages/panel/profile/profile.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoguotComponent } from './loguot/loguot.component';
-
-export const ROUTES: Routes = [
-    // { path: "", pathMatch: "full", redirectTo: "list" },
-    { path: 'loguot', component: LoguotComponent },
-];
